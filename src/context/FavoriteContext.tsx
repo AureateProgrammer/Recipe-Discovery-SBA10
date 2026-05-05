ddefbfd81a937fcc053fcb2e16ac778ad458e02d
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-05-04 23:05:19 -0400

    Creating

--- src/context/FavoriteContext.tsx
+++ src/context/FavoriteContext.tsx
@@ -9,3 +9,28 @@ interface FavoriteContextType {
 
 const FavoriteContext = createContext<FavoriteContextType | undefined>(undefined);
 
+export const FavoriteProvider: React.FC = ({ children }) => {
+    const [favorites, setFavorites] = useLocalStorage<string[]>('favorites', []);
+
+    const addFavorite = (id: string) => {
+        setFavorites(prev => [...prev, id]);
+    };
+
+    const removeFavorite = (id: string) => {
+        setFavorites(prev => prev.filter(fav => fav !== id));
+    };  
+
+
+    return (        <FavoriteContext.Provider value={{ favorites, addFavorite, removeFavorite }}>
+            {children}
+        </FavoriteContext.Provider>
+    );
+};
+
+export const useFavorite = () => {
+    const context = useContext(FavoriteContext);
+    if (!context) {
+        throw new Error('useFavorite must be used within a FavoriteProvider');
+    }
+    return context;
+};  
