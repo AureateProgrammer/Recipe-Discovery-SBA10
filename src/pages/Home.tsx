14a8a3c59e7136a68242790786f04860d99286c7
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-05-04 23:24:34 -0400

    Intergrating API

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,3 +10,26 @@ interface Meal {
 interface MealResponse {
     meals: Meal[];
 }
+
+export default function Home() {
+    const { data, loading, error } = useFetch<MealResponse>(
+        'https://www.themealdb.com/api/json/v1/1/search.php?s=chicken'
+    );
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Something went wrong!</p>;
+    return (
+        <div>
+            <h1>Meals</h1>
+            <ul>
+                {data?.meals.map(meal => (
+                    <li key={meal.idMeal}>
+                        <h2>{meal.strMeal}</h2>
+                        <p>Category: {meal.strCategory}</p>
+                        <img src={meal.strMealThumb} alt={meal.strMeal} />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
